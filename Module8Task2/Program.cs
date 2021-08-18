e3e5d237846a2093244453b17766b906d914eb0b
kema1989 <86516029+kema1989@users.noreply.github.com>
2021-08-18 12:33:12 +0600

    Добавьте файлы проекта.

---
+++ Module8Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Module8Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dirPath = @"C:\Users\YOGA\Desktop\WaitingForDeath";
+            DirectoryInfo dirSpace = new DirectoryInfo(dirPath);
+            try
+            {
+                if (dirSpace.Exists)
+                {
+                    FileInfo files = new FileInfo()
+                }
+            }
+        }
+    }
+}
